db23321d37e2a66046cc248d37cd0e89d765868c
jeje <jjeje1303@gmail.com>
2023-03-08 08:59:12 +0700

    PUSH

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,29 +12,29 @@ export default defineNuxtConfig({
       ],
       link: [
         {
-          rel: 'stylesheet', 
+          rel: 'stylesheet',
           href: base_url + 'assets/vendor/swiperjs-6.6.2/swiper-bundle.min.css'
         },
         {
-          rel: 'stylesheet', 
+          rel: 'stylesheet',
           href: 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css'
         },
-        { 
-          rel: 'stylesheet', 
+        {
+          rel: 'stylesheet',
           href: base_url + 'assets/css/style.css'
         },
-        { 
+        {
           rel: "stylesheet",
           href: "https://kit.fontawesome.com/b03824aa26.css"
         },
-        { 
+        {
           rel: "stylesheet",
           href: "https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
         }
       ],
       script: [
-        { 
-          src: base_url + 'assets/js/script' 
+        {
+          src: base_url + 'assets/js/script'
         },
         {
           src: base_url + 'assets/js/jquery-3.3.1.min.js'
@@ -65,9 +65,6 @@ export default defineNuxtConfig({
         },
         {
           src: base_url + 'assets/js/app.js'
-        },
-        {
-          src: base_url + 'assets/js/presensi.js'
         }
       ]
     }
